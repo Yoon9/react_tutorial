6c203ac6226161f7b1b990b564070e5728752c01
yoon9 <dbs852456@naver.com>
2020-10-10 17:50:13 +0900

    회원 추가 까지 구현

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,57 @@
-import React from 'react';
-import InputSample from './InputSample';
+import React, { useRef, useState } from 'react';
+import CreateUser from './CreateUser';
+import UserList from './UserList';
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+  const [users, setUsers] = useState([
+    {
+        id: 1,
+        username: '테스트1',
+        email: 'dbs852456@naver.com'
+    },
+    {
+        id: 2,
+        username: '테스트2',
+        email: 'dbs852456@2naver.com'
+    },
+    {
+        id: 3,
+        username: '테스트3',
+        email: 'dbs852456@3naver.com'
+    },
+  ]);
+  const nextId = useRef(4);
+  const onCreate = () => {
+    console.log(nextId);
+    const user = {
+      id: nextId,
+      username,
+      email,
+    }
+    setUsers(users.concat(user));
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  }
   return (
-    <InputSample />
+    <>
+    <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+    <UserList users={users} />
+    </>
   )
 }
 
--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 
 function InputSample(){
@@ -6,24 +6,26 @@ function InputSample(){
         name: '',
         nick: ''
     });
+    const nameInput = useRef();
     const {name, nickname} = inputs;
     const onChange = (e) => {
         const {name, value} = e.target;
-        const nextInputs = {
+        setInputs({
             ...inputs,
             [name] : value,
-        }
+        })
     }
     const onReset = () => {
         setInputs({
             name: '',
             nickname: ''
         });
+        nameInput.current.focus();
     }
     return(
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} />
-            <input name="nickname" placeholder="닉네임" onChange={onChange} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
+            <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>이름 : {name} / 닉네임 : {nickname}</div>
         </div>
